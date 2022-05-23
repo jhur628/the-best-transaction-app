9d00bad09d7020570f0f96872d0290a644eb4d62
John Hur <hur.john628@gmail.com>
2022-05-23 00:25:38 -0500

    fixed state

--- src/App.js
+++ src/App.js
@@ -3,19 +3,39 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 
 function App() {
-  const [ transactions, setTransactions ] = useState([]);
+  const profile = {
+    name: "Will",
+    accountNumber : 60192890,
+    startingBalance: 50000
+  }
+
+  const { name, accountNumber, startingBalance} = profile
+ 
+  const [ transactions, setTransactions ] = useState();
+
+  useEffect(() => {
+    axios.get(`https://the-best-transaction-server.herokuapp.com/`)
+      .then(res => {
+        setTransactions(res.data);
+      });
+  }, [])
 
   
-    axios.get(`https://the-best-transaction-server.herokuapp.com/`)
-      .then(res => setTransactions(res.data));
-
-  console.log(transactions);
 
   return (
     <div className="App">
       <header>
         The Best Transaction App
       </header>
+      <div>
+        Welcome back {name} ({accountNumber})!
+      </div>
+      <div>
+        Starting Balance: {startingBalance}
+      </div>
+      <div>
+        
+      </div>
 
     </div>
   );
