092451fa3ded66bbd5a67159f7b044c89be77056
John Hur <hur.john628@gmail.com>
2022-05-23 16:14:31 -0500

    able to delete transactions

--- src/components/transactionsList/TransactionList.js
+++ src/components/transactionsList/TransactionList.js
@@ -1,11 +1,15 @@
+import axios from "axios";
 
 
 export default function TransactionList({transactions}) {
-
+  const deleteTransaction = async (id) => {
+    await axios.delete(`https://the-best-transaction-server.herokuapp.com/${id}`);
+    window.location.reload();
+  }
     
   const mappedTransactions = transactions.map((transaction, i) => {
     return <div>
-      {transaction.type} {transaction.amount}
+      {transaction.type} {transaction.amount} <button onClick={() => deleteTransaction(transaction.id)}>DELETE</button>
     </div>
   })
 
